13a55360e57a4954ff458ab951067ddb7bfec042
Usmonov Asilbek <asilbekusmonovv17@gmail.com>
2026-09-14 22:30:32 +0500

    Delete test_models.py

--- test_models.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv(".env")
-import google.generativeai as genai  # noqa: E402
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-print("API KEY:", os.getenv("GEMINI_API_KEY")[:5] + "...")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
